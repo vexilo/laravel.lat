13f7134c00c7b8f24537978a23467c7539d2ec63
Alfonso Bribiesca Espinosa <alfonso@vexilo.com>
2015-11-10 21:07:36 -0600

    Borro archivo de prueba

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -1,7 +1,7 @@
 <?php namespace App\Http\Controllers;
 
 class HomeController extends Controller {
-
+s
 	/*
 	|--------------------------------------------------------------------------
 	| Home Controller
